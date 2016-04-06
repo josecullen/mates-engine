3b04817be2e57c0b2981ce150fb4fd5cc387d733
josecullen <jcullen@baufest.com>
2016-04-06 18:40:42 -0300

    Se pone focus un elemento editable -del creador de juegos del admin-, cuando se le hace click

--- math-admin/webroot/app/td-editable.component.ts
+++ math-admin/webroot/app/td-editable.component.ts
@@ -1,35 +1,73 @@
-import {Component, Input, Output, EventEmitter} from 'angular2/core';
+import {
+    Component, 
+    ElementRef, 
+    Input, 
+    Output, 
+    EventEmitter,
+    ApplicationRef,
+    NgSwitch,
+    NgSwitchWhen,
+    NgSwitchDefault
+} from 'angular2/core';
 
 @Component({
     selector: 'editable',
-    template: `
-	<div *ngIf="!edit" (click)="startEdit()">{{value}}</div>
-	<div *ngIf="edit" >
-		<input #form
-			size="6"
-			max="999"
-			min="0"
-			type="{{type}}"
-			value="{{value}}"
-			checked="{{value}}"
-			(blur)="type == 'checkbox' ? editEnd(form.checked) : editEnd(form.value)"
-			(keyup.enter)="type == 'checkbox' ? editEnd(form.checked) : editEnd(form.value)"
-			(keyup.escape)="edit = !edit">
-	</div>
-    `
+    template: `        
+            <div *ngIf="!edit" (click)="startEdit(form)">{{value}}</div>
+            <div #divForm *ngIf="edit">
+                <input #form id="editForm"
+                    size="6"
+                    max="999"
+                    min="0"
+                    type="{{type}}"
+                    value="{{value}}"
+                    checked="{{value}}"
+                    (blur)="type == 'checkbox' ? editEnd(form.checked) : editEnd(form.value)"
+                    (keyup.enter)="type == 'checkbox' ? editEnd(form.checked) : editEnd(form.value)"
+                    (keyup.escape)="edit = !edit"
+                >
+            </div>
+        
 
+    `
 })
 export class Editable {
     @Input() type: string = "number";
     @Input() value: any;
-    @Input() edit: boolean;
+    @Input() edit: boolean = false;
     @Output() valueChanged: EventEmitter = new EventEmitter();
-    edit: boolean = false;
 
-    startEdit() {
-        this.edit = !this.edit;
+    el:ElementRef;
+    _appRef:ApplicationRef;
+    constructor(el:ElementRef, _appRef: ApplicationRef){
+        this.el = el;
+        this._appRef = _appRef;
     }
 
+    setVisibility(element:any){
+        if(element == 'show'){
+            return this.edit ? "hidden" : "visible";
+        }else{
+            return this.edit ? "visible" : "hidden";
+        }
+    }
+
+
+    startEdit() {
+        this.edit = !this.edit;  
+       
+        this._appRef.tick();
+        var input:any = this.el.nativeElement.children[0].children[0];
+        console.log(input);
+        input.focus();
+        input.autofocus = true;
+
+    }
+
+    saveEl(editForm:any){
+        console.log(editForm);
+        return this.edit;
+    }
 
 
     editEnd(editedValue: any) {
